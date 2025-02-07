b8db737868f137d6f9c9928ff819068e38b6225c
Fabian Pfannschmidt <fabian.pfannschmidt@smail.th-koeln.de>
2025-02-07 10:29:53 +0100

    deleted file

--- src/router/index.js
+++ src/router/index.js
@@ -10,15 +10,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
- 
-    {
       path: '/project-id',
       name: 'projects-id',
       component: () => import('../views/FinanzierungsOption.vue'),
